5f3b934afd32b52685e14016c22887dfea4e7425
Nathan Collier <nathaniel.collier@gmail.com>
2023-08-07 11:24:51 -0400

    added some performance tests to the search via pyesgf

--- test_esgf.py
+++ test_esgf.py
@@ -1,6 +1,8 @@
-import json
 import re
+import timeit
+from functools import partial
 
+import numpy as np
 import pytest
 import requests
 from netCDF4 import Dataset
@@ -23,6 +25,56 @@ def get_page_links(page_url):
         if (not url.endswith(".ipynb") and not url.endswith("catalog.html"))
     ]
     return urls
+
+
+def check_search(esg_search: str):
+    """Given the connection for the search, use pyesgf to check search results."""
+    conn = SearchConnection(esg_search, distrib=False, timeout=600)
+    ctx = conn.new_context(
+        facets=[
+            "project",
+            "experiment_id",
+            "source_id",
+            "member_id",
+            "grid_label",
+            "frequency",
+            "variable_id",
+        ],
+        project="CMIP6",
+        experiment_id="historical",
+        source_id=["CESM2", "NorESM2-LM"],
+        member_id="r1i1p1f1",
+        grid_label="gn",
+        frequency=["mon"],
+        variable_id=["gpp"],
+        latest=True,
+    )
+    if ctx.hit_count == 0:
+        raise ValueError(f"The {esg_search} connection returns no results.")
+    results = ctx.search()
+    return results
+
+
+def check_file_core(core):
+    file_url = f"https://{core}/esg-search/search?type=File&distrib=false&limit=0&format=application%2fsolr%2bjson"
+    resp = requests.get(file_url)
+
+    numfound = resp.json()["response"]["numFound"]
+    if numfound < EXPECTED_FILES:
+        raise ValueError(
+            f"At {core}: {numfound} files found, below the {EXPECTED_FILES} threshold."
+        )
+
+
+def benchmark_search(core, typical_mean, typical_std, repeat=10):
+    times = np.array(
+        timeit.repeat(partial(check_search, core), number=1, repeat=repeat)
+    )
+    print(f"{core}  {times.mean():.2f} ± {times.std():.2f} [s]")
+    if np.abs(times.mean() - typical_mean) / typical_std > 1:
+        raise ValueError(
+            f"Search times have significantly changed for {core}, {typical_mean} ± {typical_std} [s] --> {times.mean():.2f} ± {times.std():.2f} [s]"
+        )
 
 
 @pytest.mark.ornl
@@ -83,33 +135,9 @@ def test_llnl_https_download_links():
             )
 
 
-def check_search(esg_search: str):
-    """Given the connection for the search, use pyesgf to check search results."""
-    conn = SearchConnection(esg_search, distrib=False, timeout=600)
-    ctx = conn.new_context(
-        facets=[
-            "data_node",
-            "project",
-            "experiment_id",
-            "source_id",
-            "member_id",
-            "grid_label",
-            "frequency",
-            "variable_id",
-        ],
-        data_node="esgf-node.ornl.gov" if "ornl" in conn.url else None,
-        project="CMIP6",
-        experiment_id="historical",
-        source_id=["CESM2", "NorESM2-LM"],
-        member_id="r1i1p1f1",
-        grid_label="gn",
-        frequency=["mon"],
-        variable_id=["gpp"],
-        latest=True,
-    )
-    if ctx.hit_count == 0:
-        raise ValueError(f"The {esg_search} connection returns no results.")
-    results = ctx.search()
+@pytest.mark.ornl
+def test_ornl_search():
+    results = check_search("https://esgf-node.ornl.gov/esg-search/")
     for dsr in results:
         links = [
             fr.opendap_url
@@ -122,27 +150,33 @@ def check_search(esg_search: str):
             )
 
 
-def check_file_core(core):
-    file_url = f"https://{core}/esg-search/search?type=File&distrib=false&limit=0&format=application%2fsolr%2bjson"
-    resp = requests.get(file_url)
-
-    numfound = resp.json()["response"]["numFound"]
-    if numfound < EXPECTED_FILES:
-        raise ValueError(
-            f"At {core}: {numfound} files found, below the {EXPECTED_FILES} threshold."
-        )
-
-
-@pytest.mark.ornl
-def test_ornl_search():
-    check_search("https://esgf-node.ornl.gov/esg-search/")
-
-
 @pytest.mark.llnl
 def test_llnl_search():
-    check_search("https://esgf-node.llnl.gov/esg-search")
+    results = check_search("https://esgf-node.llnl.gov/esg-search")
+    for dsr in results:
+        links = [
+            fr.opendap_url
+            for fr in dsr.file_context().search()
+            if fr.opendap_url is not None
+        ]
+        if len(links) != dsr.number_of_files:
+            raise ValueError(
+                f"{dsr.dataset_id} number of files ({len(links)}) not equal to the results number ({dsr.number_of_links})"
+            )
 
 
 @pytest.mark.ornl
 def test_ornl_file_core():
     check_file_core("esgf-node.ornl.gov")
+
+
+@pytest.mark.ornl
+@pytest.mark.performance
+def test_ornl_search_speed():
+    benchmark_search("https://esgf-node.ornl.gov/esg-search/", 1.11, 0.06)
+
+
+@pytest.mark.llnl
+@pytest.mark.performance
+def test_llnl_search_speed():
+    benchmark_search("https://esgf-node.llnl.gov/esg-search/", 0.74, 0.21)
